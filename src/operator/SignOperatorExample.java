273a9fbc8df0b2b87ba31605ea37d6ee19a9838e
parksangwoo <b01056721617@gmail.com>
2021-06-11 11:34:32 +0900

    SignOperator

---
+++ src/operator/SignOperatorExample.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class SignOperatorExample {
+    public static void main(String[] args) {
+        int x = -100;
+        int result1 = +x;
+        int result2 = -x;
+        System.out.println("result1=" + result1);
+        System.out.println("result2=" + result2);
+    }
+}
