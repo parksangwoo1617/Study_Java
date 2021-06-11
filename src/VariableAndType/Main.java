ca359de76028e584caf639d637aae5f6b5b423db
parksangwoo <b01056721617@gmail.com>
2021-06-11 10:10:08 +0900

    CREATE

---
+++ src/VariableAndType/Main.java
@@ -0,0 +1,11 @@
+package VariableAndType;
+
+public class Main {
+    pubic static void main(String[] args) {
+        int value = 10;
+
+        int result = value + 10;
+
+        System.out.println(result);
+    }
+}
