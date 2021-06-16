852776cb3f35cd63d4c0f1e4ec04be6547f9c70f
parksangwoo <b01056721617@gmail.com>
2021-06-16 19:49:15 +0900

    Computer

--- src/inherit/Computer.java
+++ src/inherit/Computer.java
@@ -1,2 +1,9 @@
-package inherit;public class Computer {
+package inherit;
+
+public class Computer extends Calculator {
+    @Override
+    double areaCircle(double r) {
+        System.out.println("Computer 객체의 areaCircle() 실행");
+        return Math.PI * r * r;
+    }
 }
