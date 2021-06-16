f2c892df3863da6f6e9c5429de1a53bfc000d5c6
parksangwoo <b01056721617@gmail.com>
2021-06-16 19:48:41 +0900

    StudentExample

--- src/inherit/StudentExample.java
+++ src/inherit/StudentExample.java
@@ -1,2 +1,10 @@
-package inherit;public class StudentExample {
+package inherit;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student student = new Student("홍길동", "1235231", 1);
+        System.out.println("name: " + student.name);
+        System.out.println("ssn: " + student.ssn);
+        System.out.println("studentNo: " + student.studentNo);
+    }
 }
