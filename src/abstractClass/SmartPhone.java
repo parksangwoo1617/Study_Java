aff39422ac24ddef2ab271f26c26c3a52aa64291
parksangwoo <b01056721617@gmail.com>
2021-06-17 16:43:35 +0900

    SmartPhoneClass

--- src/abstractClass/SmartPhone.java
+++ src/abstractClass/SmartPhone.java
@@ -1,2 +1,11 @@
-package abstractClass;public class SmartPhone {
+package abstractClass;
+
+public class SmartPhone extends Phone {
+    public SmartPhone(String owner) {
+        super(owner);
+    }
+
+    public void internetSearch() {
+        System.out.println("인터넷 검색을 합니다.");
+    }
 }
