dff8fe5ffd2e12311a1edd1e1265c684cf5eb6c4
parksangwoo <b01056721617@gmail.com>
2021-06-17 20:27:20 +0900

    AudioClass

--- src/aboutInterface/Audio.java
+++ src/aboutInterface/Audio.java
@@ -3,6 +3,7 @@ package aboutInterface;
 public class Audio implements RemoteControl {
 
     private int volume;
+    private boolean mute;
 
     public void turnOn() {
         System.out.println("Audio를 켭니다.");
@@ -20,5 +21,15 @@ public class Audio implements RemoteControl {
             this.volume = volume;
         }
         System.out.println("현재 Audio 볼륨: " + this.volume);
+    }
+
+    @Override
+    public void setMute(boolean mute) {
+        this.mute = mute;
+        if(mute) {
+            System.out.println("Audio 무음 처리합니다.");
+        } else {
+            System.out.println("Audio 무음 해제합니다.");
+        }
     }
 }
