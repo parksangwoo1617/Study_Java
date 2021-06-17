65815a7fd56d8c014704c8a3984ec7ed9d99eb2c
parksangwoo <b01056721617@gmail.com>
2021-06-17 20:27:29 +0900

    RemoteControlExampleClass

--- src/aboutInterface/RemoteControlExample.java
+++ src/aboutInterface/RemoteControlExample.java
@@ -1,11 +1,19 @@
 package aboutInterface;
 
 public class RemoteControlExample {
-    public static void main() {
+    public static void main(String[] args) {
         RemoteControl rc = new RemoteControl() {
             public void turnOn() {};
             public void turnOff() {};
             public void setVolume(int volume) {};
         };
+        RemoteControl remote;
+        remote = new Television();
+        remote.turnOn();
+        remote.setMute(true);
+
+        remote = new Audio();
+        remote.turnOn();
+        remote.setMute(true);
     }
 }
