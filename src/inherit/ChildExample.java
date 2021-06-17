082aae27a48b0cbad8f2d90bef5474331a53b820
parksangwoo <b01056721617@gmail.com>
2021-06-17 14:15:11 +0900

    ChildExample

--- src/inherit/ChildExample.java
+++ src/inherit/ChildExample.java
@@ -1,2 +1,14 @@
-package inherit;public class ChildExample {
+package inherit;
+
+public class ChildExample {
+    public static void main() {
+        Parent parent = new Child();
+        parent.field1 = "data1";
+        parent.method1();
+        parent.method2();
+
+        Child child = (Child) parent;
+        child.field2 = "yyy";
+        child.method3();
+    }
 }
