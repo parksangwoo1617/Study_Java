b6020756483d367b4d8952705e21a3f161b32d0a
parksangwoo <b01056721617@gmail.com>
2021-06-18 15:09:33 +0900

    ExampleClass

--- src/aboutInterface/Example.java
+++ src/aboutInterface/Example.java
@@ -1,2 +1,20 @@
-package aboutInterface;public class Example {
+package aboutInterface;
+
+public class Example {
+    public static void main(String[] args) {
+        ImplementationC impl = new ImplementationC();
+
+        InterfaceA ia = impl;
+        ia.methodA();
+        System.out.println();
+
+        InterfaceB ib = impl;
+        ib.methodB();
+        System.out.println();
+
+        InterfaceC ic = impl;
+        ic.methodA();
+        ic.methodB();
+        ic.methodC();
+    }
 }
