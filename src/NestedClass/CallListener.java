3b3b2d8e8b4f96dbbdb165acd3b542383eb4e44e
parksangwoo <b01056721617@gmail.com>
2021-06-18 20:01:13 +0900

    CallListenerClass

--- src/NestedClass/CallListener.java
+++ src/NestedClass/CallListener.java
@@ -1,2 +1,8 @@
-package NestedClass;public class CallListener {
+package NestedClass;
+
+public class CallListener implements Button.OnClickListener {
+    @Override
+    public void onClick() {
+        System.out.println("전화를 겁니다.");
+    }
 }
