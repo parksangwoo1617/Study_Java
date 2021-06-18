5fb969168dad6a43f8f6b644e2a3fb48dbdfe3ec
parksangwoo <b01056721617@gmail.com>
2021-06-18 15:10:22 +0900

    MyInterface

--- src/aboutInterface/MyInterface.java
+++ src/aboutInterface/MyInterface.java
@@ -1,2 +1,9 @@
-package aboutInterface;public interface MyInterface {
+package aboutInterface;
+
+public interface MyInterface {
+    public void method1();
+
+    public default void method2() {
+        System.out.println("MyInterface-method2 실행");
+    }
 }
