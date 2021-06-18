cd5c049e9354dafa97a60a6b92eca169b94a4025
parksangwoo <b01056721617@gmail.com>
2021-06-18 15:09:50 +0900

    MyClassA

--- src/aboutInterface/MyClassA.java
+++ src/aboutInterface/MyClassA.java
@@ -1,2 +1,8 @@
-package aboutInterface;public class MyClassA {
+package aboutInterface;
+
+public class MyClassA implements MyInterface {
+    @Override
+    public void method1() {
+        System.out.println("MyInterface-method2 실행");
+    }
 }
