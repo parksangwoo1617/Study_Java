fcbf0f75a4c7c5400788aa2fdb5872f595e2fcf1
parksangwoo <b01056721617@gmail.com>
2021-06-18 19:30:19 +0900

    MainClass

--- src/NestedClass/Main.java
+++ src/NestedClass/Main.java
@@ -1,2 +1,20 @@
-package NestedClass;public class Main {
+package NestedClass;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+
+        //인스턴스 멤버 클래스 객체 생성
+        A.B b = a.new B();
+        b.field1 = 3;
+        b.method1();
+
+        A.C c = new A.C();
+        c.field1 = 3;
+        c.method1();
+        A.C.field2 = 3;
+        A.C.method2();
+
+        a.method();
+    }
 }
