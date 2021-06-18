1a5855baf54d2a2f4804916f84258d3589cc3a09
parksangwoo <b01056721617@gmail.com>
2021-06-18 15:10:06 +0900

    ImplementationC

--- src/aboutInterface/ImplementationC.java
+++ src/aboutInterface/ImplementationC.java
@@ -1,2 +1,15 @@
-package aboutInterface;public class ImplementationC {
+package aboutInterface;
+
+public class ImplementationC implements InterfaceC {
+    public void methodA() {
+        System.out.println("ImplementationC-methodA() 실행");
+    }
+
+    public void methodB() {
+        System.out.println("ImplementationC-methodB() 실행");
+    }
+
+    public void methodC() {
+        System.out.println("ImplementationC-methodC() 실행");
+    }
 }
