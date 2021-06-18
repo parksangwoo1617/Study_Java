5fbe1591f2eb551354cda99a037a2c14e3cb5cdb
parksangwoo <b01056721617@gmail.com>
2021-06-18 20:01:57 +0900

    OutterClass

--- src/NestedClass/Outter.java
+++ src/NestedClass/Outter.java
@@ -1,2 +1,21 @@
-package NestedClass;public class Outter {
+package NestedClass;
+
+public class Outter {
+    String field = "Outter-field";
+    void method() {
+        System.out.println("Outter-method");
+    }
+
+    class Nested {
+        String field = "Nested-field";
+        void method() {
+            System.out.println("Nested-method");
+        }
+        void print() {
+            System.out.println(this.field);
+            this.method();
+            System.out.println(Outter.this.field);
+            Outter.this.method();
+        }
+    }
 }
