bb0bc29fa604671cde1086c48704bcd337c28cb8
parksangwoo <b01056721617@gmail.com>
2021-06-18 15:11:10 +0900

    MyClassB

--- src/aboutInterface/MyclassB.java
+++ src/aboutInterface/MyclassB.java
@@ -1,2 +1,13 @@
-package aboutInterface;public class MyclassB {
+package aboutInterface;
+
+public class MyclassB implements MyInterface {
+    @Override
+    public void method1() {
+        System.out.println("MyClassB-method1() 실행");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("MyClassB-method2() 실행");
+    }
 }
