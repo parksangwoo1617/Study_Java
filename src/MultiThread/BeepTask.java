87c6f5429b6e8519d8e912cedd8567742ec087e3
parksangwoo <b01056721617@gmail.com>
2021-06-24 21:23:31 +0900

    BeepTaskClass

--- src/MultiThread/BeepTask.java
+++ src/MultiThread/BeepTask.java
@@ -1,2 +1,13 @@
-package MultiThread;public class BeepTask {
+package MultiThread;
+
+import java.awt.*;
+
+public class BeepTask implements Runnable {
+    public void run() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i = 0; i < 5; i++) {
+            toolkit.beep();
+            try { Thread.sleep(1500); } catch(Exception e) {}
+        }
+    }
 }
