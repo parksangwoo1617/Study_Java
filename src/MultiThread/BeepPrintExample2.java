53fa01085c3be5922060524726fb9072449e24c1
parksangwoo <b01056721617@gmail.com>
2021-06-24 21:23:12 +0900

    BeepPrintExample2Class

--- src/MultiThread/BeepPrintExample2.java
+++ src/MultiThread/BeepPrintExample2.java
@@ -1,2 +1,13 @@
-package MultiThread;public class BeepPrintExample2 {
+package MultiThread;
+
+public class BeepPrintExample2 {
+    public static void main(String[] args) {
+        Thread thread = new BeepThread();
+        thread.start();
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try { Thread.sleep(500); } catch(Exception e) {}
+        }
+    }
 }
