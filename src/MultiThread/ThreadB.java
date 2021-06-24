d6a0c88d0f89c6d920b571007541bce8a839e77e
parksangwoo <b01056721617@gmail.com>
2021-06-24 22:43:42 +0900

    ThreadBClass

--- src/MultiThread/ThreadB.java
+++ src/MultiThread/ThreadB.java
@@ -1,2 +1,9 @@
-package MultiThread;public class ThreadB {
+package MultiThread;
+
+public class ThreadB extends Thread {
+    public void run() {
+        for(int i = 0; i < 2; i++) {
+            System.out.println(getName() + "가 출력한 내용");
+        }
+    }
 }
