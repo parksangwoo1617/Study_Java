0c209a446deab0c3cb16274b9d1014b902ecf763
parksangwoo <b01056721617@gmail.com>
2021-06-28 10:53:33 +0900

    CalcThreadClass

---
+++ src/MultiThread/CalcThread.java
@@ -0,0 +1,11 @@
+package MultiThread;
+
+public class CalcThread extends Thread {
+    public CalcThread(String name) {
+        setName(name);
+    }
+    public void run() {
+        for(int i = 0; i < 20000000; i++) {}
+    System.out.println(getName());
+    }
+}
