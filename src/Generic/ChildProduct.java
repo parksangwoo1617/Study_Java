b8756b38bf511aef98dd03b347e4e341a371b6b2
parksangwoo <b01056721617@gmail.com>
2021-06-30 15:56:25 +0900

    ChildProductClass

--- src/Generic/ChildProduct.java
+++ src/Generic/ChildProduct.java
@@ -1,2 +1,7 @@
-package Generic;public class ChildProduct {
+package Generic;
+
+public class ChildProduct<T, M, C> extends Product<T, M> {
+    private C company;
+    public C getCompany() { return this.company; }
+    public void setCompany(C company) { this.company = company; }
 }
