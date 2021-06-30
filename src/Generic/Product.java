fc364f88091545f9a9e120664413fa00acf79425
parksangwoo <b01056721617@gmail.com>
2021-06-30 11:57:56 +0900

    ProductClass

--- src/Generic/Product.java
+++ src/Generic/Product.java
@@ -1,2 +1,12 @@
-package Generic;public class Product {
+package Generic;
+
+public class Product<T, M> {
+    private T kind;
+    private M model;
+
+    public T getKind() { return this.kind; }
+    public M getModel() { return this.model; }
+
+    public void setKind(T kind) { this.kind = kind; }
+    public void setModel(M model) { this.model = model; }
 }
