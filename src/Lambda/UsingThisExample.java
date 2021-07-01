ef4a95310e0840f5fcae864a8be1a05bcecf9fa8
parksangwoo <b01056721617@gmail.com>
2021-07-01 09:52:56 +0900

    UsingThisExampleClass

--- src/Lambda/UsingThisExample.java
+++ src/Lambda/UsingThisExample.java
@@ -1,2 +1,9 @@
-package Lambda;public class UsingThisExample {
+package Lambda;
+
+public class UsingThisExample {
+    public static void main(String[] args) {
+        UsingThis usingThis = new UsingThis();
+        UsingThis.Inner inner = usingThis.new Inner();
+        inner.method();
+    }
 }
