fccb581c5bbfbb4c522572646cccd0516f5d629e
parksangwoo <b01056721617@gmail.com>
2021-07-01 09:52:18 +0900

    RunnableExampleClass

--- src/Lambda/RunnableExample.java
+++ src/Lambda/RunnableExample.java
@@ -1,2 +1,13 @@
-package Lambda;public class RunnableExample {
+package Lambda;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            for(int i = 0; i < 10; i++) {
+                System.out.println(i);
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
 }
