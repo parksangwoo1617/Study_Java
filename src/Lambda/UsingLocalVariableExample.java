a7295212b05a0dce776dbb0bfbdcac3066311615
parksangwoo <b01056721617@gmail.com>
2021-07-01 09:52:39 +0900

    UsingLocalVariableExampleClass

--- src/Lambda/UsingLocalVariableExample.java
+++ src/Lambda/UsingLocalVariableExample.java
@@ -1,2 +1,8 @@
-package Lambda;public class UsingLocalVariableExample {
+package Lambda;
+
+public class UsingLocalVariableExample {
+    public static void main(String[] args) {
+        UsingLocalVariable ulv = new UsingLocalVariable();
+        ulv.method(20);
+    }
 }
