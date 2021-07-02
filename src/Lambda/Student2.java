88e76d801d86f692310cf0f2ed3b0f007711c08c
parksangwoo <b01056721617@gmail.com>
2021-07-02 11:18:57 +0900

    Student2Class

--- src/Lambda/Student2.java
+++ src/Lambda/Student2.java
@@ -1,2 +1,16 @@
-package Lambda;public class Student2 {
+package Lambda;
+
+public class Student2 {
+    private String name;
+    private String sex;
+    private int score;
+
+    public Student2(String name, String sex, int score) {
+        this.name = name;
+        this.sex = sex;
+        this.score = score;
+    }
+
+    public String getSex() { return sex; }
+    public int getScore() { return score; }
 }
