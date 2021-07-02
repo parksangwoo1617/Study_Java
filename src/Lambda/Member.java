5e59ac33937a07a6bbbeec7817d43d81ad54a1fa
parksangwoo <b01056721617@gmail.com>
2021-07-02 11:17:15 +0900

    MemberClass

--- src/Lambda/Member.java
+++ src/Lambda/Member.java
@@ -1,2 +1,21 @@
-package Lambda;public class Member {
+package Lambda;
+
+public class Member {
+    private String name;
+    private String id;
+
+    public Member() {
+        System.out.println("Member() 실행");
+    }
+    public Member(String id) {
+        System.out.println("Member(String id) 실행");
+        this.id = id;
+    }
+    public Member(String name, String id) {
+        System.out.println("Member(String name, String id)");
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getId() { return id; }
 }
