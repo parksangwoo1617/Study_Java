0b6956acf1b0de8e851a11e434f5f08c84011b55
parksangwoo <b01056721617@gmail.com>
2021-07-05 19:43:26 +0900

    HashSetExample2Class

--- src/CollectionFramework/HashSetExample2.java
+++ src/CollectionFramework/HashSetExample2.java
@@ -1,2 +1,14 @@
-package CollectionFramework;public class HashSetExample2 {
+package CollectionFramework;
+
+import java.util.*;
+
+public class HashSetExample2 {
+    public static void main(String[] args) {
+        Set<Member> set = new HashSet<Member>();
+
+        set.add(new Member("홍길동", 30));
+        set.add(new Member("홍길동", 30));
+
+        System.out.println("총 객체수 : " + set.size());
+    }
 }
